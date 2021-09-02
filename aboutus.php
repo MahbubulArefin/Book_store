f22c63e5def8f3e0376f1d9692458f6f35672e85
MahbubulArefin <mak.vpd@gmail.com>
2021-09-03 00:46:28 +0600

    fina

---
+++ aboutus.php
@@ -0,0 +1,71 @@
+<?php session_start();?>
+
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+		<?php
+			include("includes/head.inc.php");
+		?>
+</head>
+
+<body>
+			<!-- start header -->
+				<div id="header">
+					<div id="menu">
+						<?php
+							include("includes/menu.inc.php");
+						?>
+					</div>
+				</div>
+				
+				<div id="logo-wrap">
+					<div id="logo">
+							<?php
+								include("includes/logo.inc.php");
+							?>
+					</div>
+				</div>
+			<!-- end header -->
+			
+			<!-- start page -->
+
+				<div id="page">
+					<!-- start content -->
+					<div id="content">
+						<div class="post">
+							<h1 class="title">About Us</h1>
+							<div class="entry" style="height:auto">
+								
+<p>
+Bookshop is an online bookstore with a mission to financially support local, independent bookstores.
+We believe that bookstores are essential to a healthy culture. They’re where authors can connect with readers, where we discover new writers, where children get hooked on the thrill of reading that can last a lifetime. They’re also anchors for our downtowns and communities.
+As more and more people buy their books online, we wanted to create an easy, convenient way for you to get your books and support bookstores at the same time.
+</p>
+							</div>
+							
+						</div>
+						
+					</div>
+					<!-- end content -->
+					
+					<!-- start sidebar -->
+					<div id="sidebar">
+							<?php
+								include("includes/search.inc.php");
+							?>
+					</div>
+					<!-- end sidebar -->
+					<div style="clear: both;">&nbsp;</div>
+				</div>
+			<!-- end page -->
+			
+			<!-- start footer -->
+				<div id="footer">
+							<?php
+								include("includes/footer.inc.php");
+							?>
+				</div>
+			<!-- end footer -->
+</body>
+</html>
---
+++ index.php
@@ -0,0 +1,95 @@
+<?php session_start();?>
+
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+		<?php
+			include("includes/head.inc.php");
+		?>
+</head>
+
+<body>
+			<!-- start header -->
+				<div id="header">
+					<div id="menu">
+						<?php
+							include("includes/menu.inc.php");
+						?>
+					</div>
+				</div>
+				
+				<div id="logo-wrap">
+					<div id="logo">
+							<?php
+								include("includes/logo.inc.php");
+							?>
+					</div>
+				</div>
+			<!-- end header -->
+			
+			<!-- start page -->
+
+				<div id="page">
+					<!-- start content -->
+					<div id="content">
+						<div class="post">
+							<h1 class="title">Welcome to 
+							<?php 
+								if(isset($_SESSION['status']))
+								{
+									echo $_SESSION['unm']; 
+								}
+								else
+								{	
+									echo 'Book Store';
+								}
+							?>
+							</h1>
+							<div class="entry">
+								<br>
+								<h1> Benefits of Reading Books:</h1>
+								<br>
+
+
+<p>
+Everything you read fills your head with new bits of information, and you never know when it might come in handy. The more knowledge you have, the better-equipped you are to tackle any challenge you’ll ever face.
+
+Additionally, here’s a bit of food for thought: should you ever find yourself in dire circumstances, remember that although you might lose everything else—your job, your possessions, your money, even your health—knowledge can never be taken from you.
+
+</p>
+
+
+
+								
+								<br>
+								
+								<br><br>
+								
+							</div>
+							
+						</div>
+						
+					</div>
+					<!-- end content -->
+					
+					<!-- start sidebar -->
+					<div id="sidebar">
+							<?php
+								include("includes/search.inc.php");
+							?>
+					</div>
+					<!-- end sidebar -->
+					<div style="clear: both;">&nbsp;</div>
+				</div>
+			<!-- end page -->
+			
+			<!-- start footer -->
+				<div id="footer">
+							<?php
+								include("includes/footer.inc.php");
+							?>
+				</div>
+			<!-- end footer -->
+</body>
+</html>
